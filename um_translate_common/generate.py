1308b9f48f3f5b7195b3a23046259285a42323cf
1F616EMO <root@1f616emo.xyz>
2023-07-31 21:33:56 +0800

    license txt

--- um_translate_common/generate.py
+++ um_translate_common/generate.py
@@ -1,7 +1,45 @@
+r"""
+    Copyright (C) 2023  1F616EMO
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+    USA
+"""
+
 import i18n
 from os import path
 
-prepend = r"""-- This file is dynamically generated.
+prepend = r"""--[[
+    Copyright (C) 2023  1F616EMO
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+    USA
+]]
+
+-- This file is dynamically generated.
 -- DO ALL THE MODIFICATIONS IN generate.py.
 local S
 if minetest.get_translator then
